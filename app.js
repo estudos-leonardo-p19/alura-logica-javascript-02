a80dd12435a0ac4a0b28833ee78298126c55400d
Leonardo Pimentel <leonardo.santos@vegus.com.br>
2024-07-15 15:48:01 -0300

    WIP: Passando valores para funções

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-let tittle = document.querySelector('h1');
-tittle.innerHTML = 'JOGO DO NÚMERO SECRETO';
 
-let paragraph = document.querySelector('p');
-paragraph.innerHTML = 'Escolha um número entre 1 e 10';
+function displayText(tag, text){
+    let field = document.querySelector(tag);
+    field.innerHTML = text;
+}
+
+
+displayText('h1', 'JOGO DO NUMERO SECRETO');
+displayText('p', 'Escolha um número entre 1 e 10');
 
 function checkAttempt(){
     console.log('O botão foi clicado');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 let tittle = document.querySelector('h1');
