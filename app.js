ce578e2742e6eb8e6aba764f3886f3fe00f9cca6
Leonardo Pimentel <leonardo.santos@vegus.com.br>
2024-07-15 13:39:03 -0300

    "WIP: Adicionando conteúdo de Texto"

---
+++ app.js
@@ -0,0 +1,5 @@
+let tittle = document.querySelector('h1');
+tittle.innerHTML = 'JOGO DO NÚMERO SECRETO';
+
+let paragraph = document.querySelector('p');
+paragraph.innerHTML = 'Escolha um número entre 1 e 10';
